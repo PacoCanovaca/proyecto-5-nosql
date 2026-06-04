317465bb609fa8a68d3176cad7afe7f802826a99
Paco <pacocanovaca97@gmail.com>
2026-06-04 18:44:18 +0200

    Traspaso de los endpoints ya creados al documento movie.routes.js del paquete routes, permitiendo organizar de esta manera las rutas y así limpiar el archivo index.js, manteniendo en él lo esencial e importando las rutas

--- index.js
+++ index.js
@@ -1,77 +1,11 @@
 const express = require("express");
 const dbConnection = require("./utils/db");
-const Movie = require("./models/Movie");
+const router = require("./routes/movie.routes");
 
 dbConnection();
 
 const PORT = 3000;
 const server = express();
-const router = express.Router();
-
-router.get("/movies", async(req, res) => {
-    try {
-        const movies = await Movie.find();
-        return res.status(200).json(movies);
-    } catch (error) {
-        return res.status(500).json(error);
-    }
-});
-
-router.get("/movies/id/:id", async(req, res) => {
-    const id = req.params.id;
-    try {
-        const movie = await Movie.findById(id);
-        if (movie) {
-            return res.status(200).json(movie);
-        } else {
-            return res.status(404).json("No movie found by this id");
-        }
-    } catch (error) {
-        return res.status(500).json(error);
-    }
-});
-
-router.get("/movies/title/:title", async(req, res) => {
-    const {title} = req.params;
-    try {
-        const movie = await Movie.findOne({title});
-        if (movie) {
-            return res.status(200).json(movie);
-        } else {
-            return res.status(404).json("No movie found by this title");
-        }
-    } catch (error) {
-        return res.status(500).json(error);
-    }
-});
-
-router.get("/movies/year/:year", async(req, res) => {
-    const {year} = req.params;
-    try {
-        const movies = await Movie.find({year});
-        if (movies.length > 0) {
-            return res.status(200).json(movies);
-        } else {
-            return res.status(404).json("No movies registered in this year");
-        }
-    } catch (error) {
-        return res.status(500).json(error);
-    }
-})
-
-router.get("/movies/genre/:genre", async(req, res) => {
-    const {genre} = req.params;
-    try {
-        const movies = await Movie.find({genre});
-        if (movies.length > 0) {
-            return res.status(200).json(movies);
-        } else {
-            return res.status(404).json("No movies found by this genre");
-        }
-    } catch (error) {
-        return res.status(500).json(error);
-    }
-})
 
 server.use("/", router);
 
